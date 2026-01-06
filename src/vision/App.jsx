09603f9b7a3101419e6f515fd3a116e3b5359206
Congthanh2004 <cthanh201@gmail.com>
2026-01-06 14:57:26 +0700

    Add files via upload

---
+++ src/vision/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AdminUsers from "./pagesAdminUsers";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">
+        HỆ THỐNG NHẬN BIẾT BIỂN BÁO GIAO THÔNG
+      </h1>
+      <AdminUsers />
+    </div>
+  );
+}
+
+export default App;
