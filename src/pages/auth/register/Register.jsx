aebe2a5c1aa2184ad6d8d2d2b2914d1656392914
Quockhang0503 <nguyenquockhang9a4@gmail.com>
2026-01-12 16:34:02 +0700

    pull

---
+++ src/pages/auth/register/Register.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { login } from "../services/authService";
+
+export default function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login(form);
+      localStorage.setItem("access", res.data.access);
+      alert("Login success");
+    } catch {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
