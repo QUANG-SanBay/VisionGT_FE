448e5b5cf6ecfa51f6cbdd2c501c7466cb4161eb
Congthanh2004 <cthanh201@gmail.com>
2026-01-16 17:25:33 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,8 @@
 // src/pages/tutor/detection/index.js
 export { default as Detection } from './customer/detection/Detection';
+
+
+export { default as AddUser } from './control/Adduser;
+export { default as EditUser } from './control/ EditUser;
+export { default as UserList } from './control/ UserList;
+export { default as ViewUser } from './control/ ViewUser;
