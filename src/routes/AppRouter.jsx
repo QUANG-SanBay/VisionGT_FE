4ad8dda3a5c9581f84c91b1260264928ab436cf9
QUANG-SanBay <quangsanbay@gmail.com>
2026-01-25 10:24:15 +0700

    feat: cấu trúc lại routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import customRoutes from "./routes/routes";
-
+import { customRoutes, adminRoutes, authRoutes } from "./routes";
 const AppRouter = () => {
   return (
     <Routes>
+      {authRoutes.map((route, index) => {
+        const Page = route.component;
+        return <Route key={index} path={route.path} element={<Page />} />;
+      })}
       {customRoutes.map((route, index) => {
+        const Page = route.component;
+        return <Route key={index} path={route.path} element={<Page />} />;
+      })}
+      {adminRoutes.map((route, index) => {
         const Page = route.component;
         return <Route key={index} path={route.path} element={<Page />} />;
       })}
